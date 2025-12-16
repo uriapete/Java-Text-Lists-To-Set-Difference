e3601d770bef4c6db900a59fd8c5c1661a680c3a
uriapete <uriarteangelo@gmail.com>
2025-12-16 14:12:41 -0800

    src/textListToSetDiff/TextListToSetDiff.java - add curr directory

--- src/textListToSetDiff/TextListToSetDiff.java
+++ src/textListToSetDiff/TextListToSetDiff.java
@@ -17,6 +17,15 @@ public class TextListToSetDiff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		File userDir = new File(System.getProperty("user.dir"));
+
+		try {
+			System.out.println(userDir.getCanonicalPath());
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.out.println("Error!");
+			System.err.println(e);
+		}
 	}
 
 }
