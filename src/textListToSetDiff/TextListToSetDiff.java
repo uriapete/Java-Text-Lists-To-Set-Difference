cb8188a96f7465e83ea883fb90478356f6c9a561
uriapete <uriarteangelo@gmail.com>
2025-12-28 02:26:55 -0800

    textListToSetDiff - format

--- src/textListToSetDiff/TextListToSetDiff.java
+++ src/textListToSetDiff/TextListToSetDiff.java
@@ -18,11 +18,14 @@ import java.util.Scanner;
 public class TextListToSetDiff {
 
 	/**
-	 * Takes a text file containing a list of string items, turns the list in that file into a HashSet.
-	 * @param txtFile The file which contains a list of strings separated by new lines.
+	 * Takes a text file containing a list of string items, turns the list in that
+	 * file into a HashSet.
+	 * 
+	 * @param txtFile The file which contains a list of strings separated by new
+	 *                lines.
 	 * @return A set containing the contents of the original text file.
 	 */
-	public static HashSet<String> fileToSet(File txtFile){
+	public static HashSet<String> fileToSet(File txtFile) {
 		// try with file scanner - exception on txtFile failure
 		try (Scanner fileScnr = new Scanner(txtFile)) {
 			// here's our returning set
@@ -31,7 +34,7 @@ public class TextListToSetDiff {
 			// while there are more lines in the file...
 			while (fileScnr.hasNextLine()) {
 				// take the next line
-				String ln =  fileScnr.nextLine();
+				String ln = fileScnr.nextLine();
 
 				// try to add the next item
 				try (Scanner lnScnr = new Scanner(ln)) {
@@ -41,7 +44,7 @@ public class TextListToSetDiff {
 
 			// return the result
 			return retSet;
-		} 
+		}
 		// catch and print error
 		catch (Exception e) {
 			System.err.println(e);
@@ -53,22 +56,22 @@ public class TextListToSetDiff {
 
 	/**
 	 * Gets the next file as directed by Scanner input.
-	 * @param inScn The input scanner.
+	 * 
+	 * @param inScn       The input scanner.
 	 * @param searchInDir What directory to search in.
 	 * @return The specified file by user or null if failure.
 	 */
-	public static File getNextFile(Scanner inScn, File searchInDir){	
+	public static File getNextFile(Scanner inScn, File searchInDir) {
 		// array to store search results
 		File[] results;
 
 		// get the name of the next file
 		String s = inScn.next();
-		
 
 		// no results? file does not exist
 		// >1 result? ...impossible case, all files must have unique name
 		results = searchInDir.listFiles((File _, String name) -> name.equals(s));
-		if (results.length!=1) {
+		if (results.length != 1) {
 			System.out.println("Invalid name! File does not exist. Please try again.");
 			return null;
 		}
@@ -77,13 +80,12 @@ public class TextListToSetDiff {
 		try {
 			System.out.println("captured " + results[0].getCanonicalPath() + "!");
 			return results[0];
-			
+
 		} catch (IOException e) {
 			System.err.println("Error in getting path of file, but surely it should exist! Please try again!");
 			System.err.println(e);
 			return null;
 		}
-
 
 	}
 
@@ -91,13 +93,11 @@ public class TextListToSetDiff {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		// how many files to subtract from our first set/list?
 		final int AMT_FILTER_FILES = 1;
 
 		// how many files will we process in total?
-		final int AMT_FILES = AMT_FILTER_FILES+1;
+		final int AMT_FILES = AMT_FILTER_FILES + 1;
 
 		// prepare scanner for console
 		Scanner cnslScanner = new Scanner(System.in);
@@ -115,7 +115,7 @@ public class TextListToSetDiff {
 
 		// Testing by outputting what directory we're in
 		try {
-			System.out.println("Searching in: "+userDir.getCanonicalPath());
+			System.out.println("Searching in: " + userDir.getCanonicalPath());
 		}
 		// If the above test didn't work, something is wrong and we can't continue!
 		catch (IOException e) {
@@ -123,16 +123,16 @@ public class TextListToSetDiff {
 			System.err.println(e);
 			return;
 		}
-		
+
 		// picking our first, biggest file to filter out
-		while (filesToCompare.size()<1) {
+		while (filesToCompare.size() < 1) {
 			System.out.println("Enter the name of the minuend and press enter file... (include file extension)");
 
 			// get the file the user wants to use
 			File result = TextListToSetDiff.getNextFile(cnslScanner, userDir);
 
 			// if null, retrieval failed and message was printed by getNextFile
-			if (result==null) {
+			if (result == null) {
 				continue;
 			}
 
@@ -147,7 +147,7 @@ public class TextListToSetDiff {
 		while (!nextFile.equals(finishString)) {
 			// call for file name the same way as above
 			System.out.println("Enter the name of the next subtrahend and press enter... (include file extension)");
-			System.out.println("Or enter '" + finishString +"'");
+			System.out.println("Or enter '" + finishString + "'");
 
 			nextFile = cnslScanner.next();
 
@@ -158,7 +158,7 @@ public class TextListToSetDiff {
 			File result = TextListToSetDiff.getNextFile(new Scanner(nextFile), userDir);
 
 			// if null, retrieval failed and message was printed by getNextFile
-			if (result==null) {
+			if (result == null) {
 				continue;
 			}
 
@@ -187,7 +187,6 @@ public class TextListToSetDiff {
 		try {
 			Files.write(Paths.get("result.txt"), result, StandardCharsets.UTF_8);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
